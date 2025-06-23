16e6e6dbcf4b370271f22347326b3a6ecd7ffb76
Rafaela Simili <rafaelasimililima@gmail.com>
2025-06-23 09:05:42 -0300

    Trabalhando com Condições

---
+++ backend/logicaProgramacao/02_condicionais.py
@@ -0,0 +1,7 @@
+nota = int(input("digite sua nota:"))
+if (nota >= 80):
+    print("Parabens")
+elif (nota < 80 and nota > 60):
+    print("Pode melhorar")
+elif (nota < 60):
+    print("Recuperacao")
