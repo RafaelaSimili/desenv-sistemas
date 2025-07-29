c4c073f96846e3dd0293c75ff2ea5df78461a486
Rafaela Simili <rafaelasimililima@gmail.com>
2025-07-29 09:38:28 -0300

    exercicio de laco

--- backend/logicaProgramacao/05_lacoderepeticao.py
+++ backend/logicaProgramacao/05_lacoderepeticao.py
@@ -3,3 +3,28 @@ for i in range(21):
     soma += i
 print("soma dos valores:" , soma)
 
+
+valor = 1
+soma = 0 
+while valor != 0:
+    valor = (int(input("digitar um valor")))
+    soma+= valor
+    print("soma:", soma) 
+
+
+senha_correta = input("configure sua senha: ")
+senha = input("digite a senha: ")
+
+while senha != senha_correta: 
+    print("senha invalida")
+    senha = input("digite a senha novamente: ")
+print("senha correta: ")
+
+
+
+
+
+
+
+
+
