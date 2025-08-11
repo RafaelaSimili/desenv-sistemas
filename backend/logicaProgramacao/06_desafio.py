9a89916e54510c1893206ab17d673a1dcae8db4d
rafaela <rafaelasimililima@gmail.com>
2025-08-11 09:01:51 -0300

    06_desafio.py

---
+++ backend/logicaProgramacao/06_desafio.py
@@ -0,0 +1,29 @@
+# Criar função de menu de opções e pedir a opção desejada ao usúario
+# Criar função de depósito
+# Criar função de saque
+# Criar função de ver saldo
+# Ao digitar "sair" encerrar o progrma
+
+def menu():
+    print("======= Menu de Opções =======\n")
+    print("1 - Depósito")
+    print("2 - Saque")
+    print("3 - Ver saldo")
+    print("Sair\n")
+    while opção != "sair":
+
+
+        opção
+
+def depositar(saldo):
+    deposito = input("valor do depósito: ", deposito) 
+    saldo =+ deposito
+
+
+
+
+   
+
+
+
+
