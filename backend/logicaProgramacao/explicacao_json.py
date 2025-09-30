d00ae5ab5bb945fa8207cfa3a905f8c35cc51151
Rafaela Simili <rafaelasimililima@gmail.com>
2025-09-30 09:44:56 -0300

    explicacao_json.py

---
+++ backend/logicaProgramacao/explicacao_json.py
@@ -0,0 +1,44 @@
+# {} -> chaves: definir um objeto - ficha de cadastro
+# [] -> colchete: definir uma lista 
+# chave/valor: descreve o valor
+
+# Sempre importar o json
+
+import json
+inventario = []
+# Lendo o arquivo
+
+try:
+    with open("loja.json", "r") as arquivo:
+        inventario = json.load (arquivo)
+
+except FileNotFoundError:
+        print("arquivo não encontrado")                                                         
+
+try:
+
+    nome = input ("digite o nome do produto: ")
+    quantidade = int(input ("digite a quantidade: "))
+    preco = float (input("digite o preço: "))
+
+except ValueError:
+       print("digite o valor corretamente")
+
+# Montar o objeto
+
+novo_produto = {"nome": nome,
+                "quantidade": quantidade,
+                "preco": preco,
+                "em_estoque": quantidade > 0 # expressão verdadeiro falso
+                }
+
+# Escrever o objeto no arquivo
+
+inventario.append (novo_produto)
+with open ("loja.json", "w") as arquivo:
+    json.dump (inventario, arquivo, indent = 4)
+    # indent -> formatar o arquivo json
+    print("produto cadastrado com sucesso")
+
+
+            
