ee056d8c4c64dcaba9e395fce38ae1fc36696660
Rafaela Simili <rafaelasimililima@gmail.com>
2025-10-06 08:53:16 -0300

    explicacao_json.py

--- backend/logicaProgramacao/explicacao_json.py
+++ backend/logicaProgramacao/explicacao_json.py
@@ -16,17 +16,19 @@ except FileNotFoundError:
         print("arquivo não encontrado")                                                         
 
 try:
-
+    id = int(input("digite o id do produto"))
     nome = input ("digite o nome do produto: ")
     quantidade = int(input ("digite a quantidade: "))
     preco = float (input("digite o preço: "))
 
 except ValueError:
-       print("digite o valor corretamente")
+    print("digite o valor corretamente")
 
 # Montar o objeto
 
-novo_produto = {"nome": nome,
+novo_produto = {
+                "id": id,
+                "nome": nome,
                 "quantidade": quantidade,
                 "preco": preco,
                 "em_estoque": quantidade > 0 # expressão verdadeiro falso
@@ -39,6 +41,41 @@ with open ("loja.json", "w") as arquivo:
     json.dump (inventario, arquivo, indent = 4)
     # indent -> formatar o arquivo json
     print("produto cadastrado com sucesso")
+
+# ----- Modificar produto -----
+
+id_produto_modificar = int(input("digite o id para modificar"))
+nova_quantidade = int(input("digite a nova quantidade"))
+
+try:
+    with open ("loja.json", "r") as arquivo:
+        inventario = json.load (arquivo)
+except FileNotFoundError:
+    print("arquivo não encontrado")  
+
+produto_encontrado = False
+
+for produto in inventario:
+    if produto['id'] = = id_produto_modificar:
+        # colocamos a nova quantidade
+        produto['quantidade'] = nova_quantidade
+        produto['em_estoque'] = nova_quantidade > 0 
+        produto_encontrado = True
+        break;
+
+if not produto_encontrado:
+    print("o produto com o id informado não foi encontrado")
+
+else:
+    with open("loja.json", "w") as arquivo:
+        json.dump(inventario, arquivo, indent = 4)
+    print("o arquivo foi alterado com sucesso!!")
+
+
+
+
+
+
 
 
             
