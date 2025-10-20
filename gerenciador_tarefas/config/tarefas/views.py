7968dd13007494e5bfe3e5411b8b713589023ee8
Rafaela Simili <rafaelasimililima@gmail.com>
2025-10-20 08:55:31 -0300

    continuando atividade django

--- gerenciador_tarefas/config/tarefas/views.py
+++ gerenciador_tarefas/config/tarefas/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Tarefa
+from django.http HttpResponse
 
-# Create your views here.
+def listar_tarefas(request):
+    tarefas_salvas = Tarefa.objects.all()
+    contexto = {
+               "minhas_tarefas": tarefas_salvas
+               }
+    return render (request, 'tarefas/lista.html', contexto)
+
+   
