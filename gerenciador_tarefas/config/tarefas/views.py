8437fc7b47d10896f1838735871511cf991846b9
Rafaela Simili <rafaelasimililima@gmail.com>
2025-10-21 09:36:09 -0300

    continuando atividade django

--- gerenciador_tarefas/config/tarefas/views.py
+++ gerenciador_tarefas/config/tarefas/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Tarefa
-from django.http HttpResponse
+from django.http import HttpResponse
 
 def listar_tarefas(request):
     tarefas_salvas = Tarefa.objects.all()
@@ -9,4 +9,9 @@ def listar_tarefas(request):
                }
     return render (request, 'tarefas/lista.html', contexto)
 
-   
+def detalhe_tarefa(request, tarefa_id):
+        
+    tarefa = get_object_or_404(Tarefa, pk=tarefa_id)
+
+    return render(request, 'tarefas/detalhe.html', {'tarefa': tarefa})
+        
