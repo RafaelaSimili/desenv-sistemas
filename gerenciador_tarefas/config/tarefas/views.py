a3780b08cd69a165386fcc0599eedc195fd39b38
Rafaela Simili <rafaelasimililima@gmail.com>
2025-10-27 08:51:30 -0300

    adicionando tarefas

--- gerenciador_tarefas/config/tarefas/views.py
+++ gerenciador_tarefas/config/tarefas/views.py
@@ -14,4 +14,15 @@ def detalhe_tarefa(request, tarefa_id):
     tarefa = get_object_or_404(Tarefa, pk=tarefa_id)
 
     return render(request, 'tarefas/detalhe.html', {'tarefa': tarefa})
+
+ def adicionar_tarefa(request):
+    if request.method = = "POST":
+        titulo = request.POST.get('titulo')
+        descriçao = request.POST.get('descriçao')
+        Tarefa.objects.create(titulo = titulo, descriçao = descriçao)
+    return redirect('lista_tarefas')
+
+
+
+
         
